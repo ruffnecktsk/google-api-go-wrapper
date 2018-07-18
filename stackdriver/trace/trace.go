70739fa05ad9f445c3b1e22b1174358206f6401a
Mikhail Kirsanov <ruffnecktsk@gmail.com>
2018-07-19 02:04:19 +0600

    Update trace.go

--- stackdriver/trace/trace.go
+++ stackdriver/trace/trace.go
@@ -7,7 +7,7 @@ import (
 	SDK "google.golang.org/api/cloudtrace/v1"
 	"google.golang.org/api/option"
 
-	"github.com/evalphobia/google-api-go-wrapper/config"
+	"github.com/ruffnecktsk/google-api-go-wrapper/config"
 )
 
 const (
